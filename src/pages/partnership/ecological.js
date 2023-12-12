c8effc86f391316cc9113e7e60f38e9f01d56ead
usamasaeed1028 <usamasaeed1028@gmail.com>
2023-12-12 22:34:38 +0500

    fix: deployment issue!

--- src/pages/partnership/ecological.js
+++ src/pages/partnership/ecological.js
@@ -1,33 +1,42 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-// import Partnership from "@/components/Home/Partnership";
-import Layout from "@/components/Layout/Layout";
+// // import Partnership from "@/components/Home/Partnership";
+// import Layout from "@/components/Layout/Layout";
 
-// import { fetchPartnersWithFilter } from "api";
+// // import { fetchPartnersWithFilter } from "api";
+
+// const ecological = () => {
+//   // const [partners, setPartners] = useState([]);
+
+//   // useEffect(() => {
+//   //   getData();
+//   // }, []);
+
+//   // const getData = async () => {
+//   //   try {
+//   //     let partnersResponse = await fetchPartnersWithFilter("ecological");
+//   //     setPartners(partnersResponse?.data?.data);
+//   //   } catch (error) {
+//   //     console.log(error);
+//   //   }
+//   // };
+//   return (
+//     <Layout>
+//       // <Partnership
+//       //   partners={partners}
+//       //   className="top-[130px] lg:top-[140px] pb-[100px]"
+//       // />
+//     </Layout>
+//   );
+// };
+
+// export default ecological;
+import React from 'react'
 
 const ecological = () => {
-  // const [partners, setPartners] = useState([]);
-
-  // useEffect(() => {
-  //   getData();
-  // }, []);
-
-  // const getData = async () => {
-  //   try {
-  //     let partnersResponse = await fetchPartnersWithFilter("ecological");
-  //     setPartners(partnersResponse?.data?.data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
   return (
-    <Layout>
-      // <Partnership
-      //   partners={partners}
-      //   className="top-[130px] lg:top-[140px] pb-[100px]"
-      // />
-    </Layout>
-  );
-};
+    <div></div>
+  )
+}
 
-export default ecological;
+export default ecological
